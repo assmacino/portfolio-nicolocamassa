94a166969117949a1528dba45b33c1afae86624a
assmacino <giunglasss@gmail.com>
2025-11-24 13:22:24 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Parkinsans, Fustat } from "next/font/google";
+import "./globals.css";
+
+import { Header } from '@/components/layout/header'
+
+const parkinsans = Parkinsans({
+  variable: "--font-parkinsans-sans",
+  subsets: ["latin"],
+});
+
+const fustat = Fustat({
+  variable: "--font-fustat-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Nicolò Camassa - Portfolio",
+  description: "Il mio portfolio personale dove vedere il mio lavoro", /* TODO: Cambiare i metdata */
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${parkinsans.variable} ${fustat.variable} antialiased`}
+      >
+        <Header />
+        <div className="xl:mx-[180px]">
+          {children}
+        </div>
+        
+      </body>
+    </html>
+  );
+}
---
+++ components/layout/header.tsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return <header></header>
+}
+
+export function HeaderMobile(){
+    return <header></header>
+}
